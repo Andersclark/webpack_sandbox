01139d2e48b1dd8049b00c5385f745f45581c408
Andersclark <anders.clark@gmail.com>
2020-02-15 20:25:33 +0100

    React and Eslint(AirBnb) implemented * React with Babel and Webpack-plugins for JS, JSX, CSS and misc files added. * Eslint set up with AirBnB-config for React. * Babel configured for React.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,80 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    }
+  mode: 'development',
+  entry: {
+    app: './src/main.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, './'), // where dev server will look for static files, not compiled
+    publicPath: '/', // relative path to output path where  devserver will look for compiled files
+    hot: true,
+  },
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'), // base path where to send compiled assets
+    publicPath: '/', // base path where referenced files will be look for
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // shortcut to reference src folder from anywhere
+    },
+  },
+  module: {
+    rules: [
+      { // config for es6 jsx
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      { // config for sass compilation
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      { // config for images
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+      { // config for fonts
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ // plugin for inserting scripts into html
+      template: './src/index.html',
+      filename: 'index.html',
+      title: 'Learning Webpack',
+    }),
+    new MiniCssExtractPlugin({ // controlling how compiled css will be outputted and named
+      filename: 'css/[name].css',
+      chunkFilename: 'css/[id].css',
+    }),
+  ],
 };
