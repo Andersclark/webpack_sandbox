ad84594d0fbcd01be7ac7019703b331c14bf84f0
Andersclark <anders.clark@gmail.com>
2020-02-16 14:05:40 +0100

    changed eslintrc to .js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "plugins": [ "react" ],
+    "extends": ["airbnb", "airbnb/hooks"],
+    "parser": "babel-eslint",
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "no-console": 0,
+      }
+  };
